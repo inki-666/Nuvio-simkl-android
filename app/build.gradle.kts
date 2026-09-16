f199118ca5e9f110f45c16b67724d85ae93452e0
inki-666 <inki5491@gmail.com>
2026-09-16 18:36:05 +0530

    Add OkHttp dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
