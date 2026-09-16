cacde21b48ed9a26063b9cb29974bf3be115b04a
inki-666 <inki5491@gmail.com>
2026-09-16 18:34:44 +0530

    Add Kotlin options for JVM target version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "com.nuvio.simkl.android"
+
     compileSdk = 35
 
     defaultConfig {
@@ -18,6 +19,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     buildFeatures {
